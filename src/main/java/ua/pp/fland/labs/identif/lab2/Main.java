8622484ff61eaa3aba19592a68e7301bc09d3e52
Maxim Bondarenko <gaus.fland@gmail.com>
2011-10-24 19:30:27 +0300

    + code refactoring

--- src/main/java/ua/pp/fland/labs/identif/lab2/Main.java
+++ src/main/java/ua/pp/fland/labs/identif/lab2/Main.java
@@ -1,9 +1,7 @@
 package ua.pp.fland.labs.identif.lab2;
 
-import org.apache.commons.math.linear.RealMatrix;
 import org.apache.log4j.Logger;
 import ua.pp.fland.labs.identif.lab2.gui.MainWindow;
-import ua.pp.fland.labs.identif.lab2.model.LeastSquareMethod;
 
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
@@ -18,32 +16,6 @@ public class Main {
 
     public static void main(String[] args) throws InvocationTargetException, InterruptedException, ClassNotFoundException, UnsupportedLookAndFeelException, IllegalAccessException, InstantiationException {
         log.debug("App started ...");
-
-        String input = "{{4, 2, 1}, {2, 4, 3}, {4, 9, 7}, {2, 2, 4}, {2, 3, 1}}";
-//        input.
-        String values[] = input.split("\\}");
-        for (int i = 0; i < values.length; i++) {
-            values[i] = values[i].replace("{{", "");
-            values[i] = values[i].replace(", {", "");
-        }
-
-        double[][] temp = new double[5][3];
-        for (int i = 0; i < values.length; i++) {
-            String row[] = values[i].split(", ");
-            for (int j = 0; j < row.length; j++) {
-                temp[i][j] = Double.valueOf(row[j]);
-            }
-        }
-
-        log.debug(values);
-
-        double[][] xValues = new double[][]{{4, 2, 1}, {2, 4, 3}, {4, 9, 7}, {2, 2, 4}, {2, 3, 1}};
-        double[][] yValues = new double[][]{{20.8}, {14.2}, {32.3}, {11.5}, {8.2}};
-
-        LeastSquareMethod leastSquareMethod = new LeastSquareMethod(xValues, yValues);
-        RealMatrix res = leastSquareMethod.process();
-
-        double[] weights = new double[]{1.1, 0.9, 1.2, 0.9, 0.9, 0.7};
 
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         SwingUtilities.invokeAndWait(new Runnable() {
